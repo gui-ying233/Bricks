8f73e843ab74cdc1c732e7743cb356c741915824
gui-ying233 <zhuyufei20050307@gmail.com>
2024-02-14 05:11:44 -0500

    草

--- test.lua
+++ test.lua
@@ -3,7 +3,7 @@ local b = require('bricks')
 local document = b:new('<h1 id="top" class="heading heading-1">Hello!</h1>\
 <div style="margin: 0 auto;" class="container">\
 	<p>\
-		My name is <dfn>Bricks</dfn>, a module for building HTML.<br>\
+		My name is <dfn>Bricks</dfn>, a module for parsing HTML.<br>\
 		Thank you for your visit.\
 	</p>\
 	<p>visitors:</p>\
